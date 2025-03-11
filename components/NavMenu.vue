785112128164dd6b7e1c9d33489c0bcd94953bd2
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-11 15:20:09 +0100

    style: Added main logo

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,10 +1,9 @@
 <template>
   <nav class="bg-indigo-500 w-56 h-screen flex flex-col items-center p-6">
-    <img
-      src="https://placehold.co/150"
-      alt="Logo du cahier de texte"
-      class="mb-10"
-    />
+    <NuxtLink to="/" class="mb-5 mt-4"
+      ><img src="/logo.svg" alt="Logo du cahier de texte" class="w-24"
+    /></NuxtLink>
+
     <NuxtLink
       to="/"
       class="text-white font-semibold hover:underline mt-5 text-xl text-center"
