7c446720c381400c038266b798493424bc3e2b18
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-11 14:43:11 +0100

    style: Fixed menu margin

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,6 +1,10 @@
 <template>
   <nav class="bg-indigo-500 w-56 h-screen flex flex-col items-center p-6">
-    <img src="https://placehold.co/150" alt="Logo du cahier de texte" />
+    <img
+      src="https://placehold.co/150"
+      alt="Logo du cahier de texte"
+      class="mb-10"
+    />
     <NuxtLink
       to="/"
       class="text-white font-semibold hover:underline mt-5 text-xl text-center"
