5ec9c1909f0588cc6177516dd67dfee523cfc184
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-11 15:15:34 +0100

    style: Added icons and stylized index.vue

---
+++ components/NotificationBell.vue
@@ -0,0 +1,12 @@
+<template>
+  <span>
+    <img v-if="new" src="/icons/bell_new.svg" />
+    <img v-else src="/icons/bell.svg" />
+  </span>
+</template>
+
+<script setup>
+defineProps({
+  new: Boolean,
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Bienvenue sur le dashboard !</h1>
+  <div class="my-5 mx-8 h-12 flex items-center justify-between w-full">
+    <h1 class="text-4xl font-bold">Dashboard</h1>
+    <div class="flex">
+      <NotificationBell :new="true"></NotificationBell>
+
+      <NuxtLink to="/account" class="pl-2">
+        <img src="/icons/account.svg" />
+      </NuxtLink>
+    </div>
+  </div>
 </template>
