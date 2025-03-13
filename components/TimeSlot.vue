9cee5c7df4d5f64d9d3222da3f68160c5963880e
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-13 09:29:46 +0100

    feat: Added Separato and TimeSlot components

---
+++ components/TimeSlot.vue
@@ -0,0 +1,28 @@
+<template>
+  <div
+    class="bg-indigo-500 mt-2 w-11/12 h-22 rounded-2xl text-white flex items-center px-2"
+  >
+    <span class="uppercase font-bold text-2xl mx-4">{{ time }}</span>
+    <span class="border-r border-white h-8/12"></span>
+    <div class="mx-4 w-full">
+      <p
+        class="font-bold text-2xl whitespace-nowrap overflow-hidden w-full overflow-ellipsis"
+      >
+        {{ title }}
+      </p>
+      <p
+        class="font-semibold text-white opacity-80 text-xl whitespace-nowrap overflow-hidden w-full overflow-ellipsis"
+      >
+        {{ subTitle }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  time: String,
+  title: String,
+  subTitle: String,
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
 <template>
   <TopNav :notif="true">Dashboard</TopNav>
-  index
+  <div
+    class="max-w-6xl h-96 border rounded-4xl py-6 flex items-center flex-col"
+  >
+    <h2 class="font-bold text-2xl">Prochains rendus</h2>
+    <Separator>Lundi 24 mars</Separator>
+    <TimeSlot
+      time="12h30"
+      title="WR412 - Rendre les travaux sur le VPS"
+      sub-title="R. Delon | Rendu sur VPS"
+    />
+  </div>
 </template>
