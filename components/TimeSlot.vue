b541dc8eff87a11386a55c5fbf509b0ab6ba4f20
DraayZe <lenny.fernet7@gmail.com>
2025-03-18 16:43:08 +0100

    Ajouter des actions dans le dashboard
    Bouton ajouter et historique dans la page principale et modification taille des textes

--- components/TimeSlot.vue
+++ components/TimeSlot.vue
@@ -2,16 +2,16 @@
   <div
     class="bg-indigo-500 mt-2 w-11/12 h-22 rounded-2xl text-white flex items-center px-2"
   >
-    <span class="uppercase font-bold text-2xl mx-4">{{ time }}</span>
+    <span class="uppercase font-bold text-xl mx-4">{{ time }}</span>
     <span class="border-r border-white h-8/12"></span>
     <div class="mx-4 w-full">
       <p
-        class="font-bold text-2xl whitespace-nowrap overflow-hidden w-full overflow-ellipsis"
+        class="font-bold text-xl whitespace-nowrap overflow-hidden w-full overflow-ellipsis"
       >
         {{ title }}
       </p>
       <p
-        class="font-semibold text-white opacity-80 text-xl whitespace-nowrap overflow-hidden w-full overflow-ellipsis"
+        class="font-semibold text-white opacity-80 text-lg whitespace-nowrap overflow-hidden w-full overflow-ellipsis"
       >
         {{ subTitle }}
       </p>
