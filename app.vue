077d3728567b3e430646a8c49c751b4a9a36b590
DraayZe <lenny.fernet7@gmail.com>
2025-03-19 10:40:40 +0100

    Début page inscription
    changement dans app.vue pour cacher la navbar de gauche quand on est sur les pages inscription et connexion

--- app.vue
+++ app.vue
@@ -1,8 +1,18 @@
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const noNavRoutes = ['/compte/inscription', '/compte/connexion']
+const hideNavMenu = noNavRoutes.includes(route.path)
+</script>
+
 <template>
   <div>
     <NuxtLayout>
       <div class="flex">
-        <NavMenu />
+        <template v-if="!hideNavMenu">
+          <NavMenu />
+        </template>
         <div class="mx-12 w-full">
           <NuxtPage />
         </div>
