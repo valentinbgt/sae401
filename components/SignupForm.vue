0a7697d22a47d02c5947b501f3903334754c5331
DraayZe <lenny.fernet7@gmail.com>
2025-03-19 12:20:42 +0100

    Fin page inscription

--- components/SignupForm.vue
+++ components/SignupForm.vue
@@ -1,46 +1,48 @@
 <template>
-  <h2>Inscripton</h2>
-  <form @submit.prevent="submitSignup" class="w-full">
-    <div class="space-y-4">
+  <div class="mx-14">
+    <h2 class="font-bold text-4xl mb-10">Inscription</h2>
+    <form @submit.prevent="submitSignup" class="w-full">
+      <div class="space-y-10">
 
-      <div class="flex">
-        <div class="flex flex-col w-30" >
-          <label class="font-bold" for="prenom">Prénom</label>
-          <input class="border rounded-lg" type="text" id="prenom" v-model="formData.prenom" required />
+        <div class="flex justify-between">
+          <div class="flex flex-col w-60" >
+            <label class="font-bold" for="prenom">Prénom</label>
+            <input class="border rounded-lg py-0.5 p-2" type="text" id="prenom" v-model="formData.prenom" required />
+          </div>
+
+          <div class="flex flex-col w-60 ">
+            <label class="font-bold" for="nom">Nom</label>
+            <input class="border rounded-lg py-0.5 p-2" type="text" id="nom" v-model="formData.nom" required />
+          </div>
         </div>
 
-        <div class="flex flex-col w-30">
-          <label class="font-bold" for="nom">Nom</label>
-          <input class="border rounded-lg" type="text" id="nom" v-model="formData.nom" required />
+        <div class="flex flex-col w-full">
+          <label class="font-bold" for="email">Email</label>
+          <input class="border rounded-lg py-0.5 p-2" type="email" id="email" v-model="formData.email" required />
         </div>
-      </div>
 
-      <div>
-        <label class="font-bold" for="email">Email</label>
-        <input class="border rounded-lg" type="email" id="email" v-model="formData.email" required />
-      </div>
+        <div class="flex flex-col w-full">
+          <label class="font-bold" for="password">Mot de passe</label>
+          <input
+              type="password"
+              id="password"
+              v-model="formData.password"
+              required
+              class="border rounded-lg py-0.5 p-2"
+          />
+        </div>
 
-      <div>
-        <label class="font-bold" for="password">Mot de passe</label>
-        <input
-            type="password"
-            id="password"
-            v-model="formData.password"
-            required
-            class="border rounded-lg"
-        />
-      </div>
+        <div v-if="error" class="text-red-500">
+          {{ error }}
+        </div>
 
-      <div v-if="error" class="text-red-500">
-        {{ error }}
+        <button class="border w-full bg-indigo-500 text-white rounded-lg py-1 cursor-pointer" type="submit" :disabled="loading">
+          {{ loading ? "Inscription en cours..." : "S'inscrire" }}
+        </button>
       </div>
-
-      <button type="submit" :disabled="loading">
-        {{ loading ? "Inscription en cours..." : "S'inscrire" }}
-      </button>
-    </div>
-    <div>Déjà un compte ? <NuxtLink to="connexion">Se connecter</NuxtLink></div>
-  </form>
+      <div class="mt-4">Déjà un compte ? <NuxtLink class="font-bold" to="connexion">Se connecter</NuxtLink></div>
+    </form>
+  </div>
 </template>
 
 <script setup>
