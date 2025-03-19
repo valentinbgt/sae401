dd18c15cecf4311a2de6febbc114695961e02d0b
Jean-Michel Le <jean-michel.le@etudiant.univ-reims.fr>
2025-03-19 18:13:13 +0100

    misa jour
    a

--- pages/dates/nouvelle.vue
+++ pages/dates/nouvelle.vue
@@ -1,5 +1,6 @@
 <template>
   <TopNav :notif="true">Nouvelle Date</TopNav>
+
   <div class="relative overflow-hidden  w-full  h-screen ">
               <div class="absolute z-10 bottom-9 -right-10">
                   <img 
@@ -8,9 +9,9 @@
                       class="w-100"
                   />
               </div>
-      <div class="flex justify-start">
-        <DeadlineForm />
-      </div>
+  <div class="flex justify-start">
+    <DeadlineForm />
   </div>
+</div>
   
 </template>
