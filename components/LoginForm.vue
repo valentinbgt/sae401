39b7973796d49adc9eff1f55080a6844527d11df
Valentin BEAUGET <164057524+valentinbgt@users.noreply.github.com>
2025-03-19 15:43:57 +0100

    fix: Signup link from login

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -8,45 +8,45 @@
             Adresse mail universitaire
           </label>
           <input
-              type="email"
-              id="email"
-              v-model="formData.email"
-              required
-              class="w-full py-2 p-2 border rounded-lg mb-4"
+            type="email"
+            id="email"
+            v-model="formData.email"
+            required
+            class="w-full py-2 p-2 border rounded-lg mb-4"
           />
         </div>
         <div>
-          <label for="password" class="font-bold">
-            Mot de passe
-          </label>
+          <label for="password" class="font-bold"> Mot de passe </label>
           <input
-              type="password"
-              id="password"
-              v-model="formData.password"
-              required
-              class="w-full py-2 p-2 border rounded-lg mb-4"
+            type="password"
+            id="password"
+            v-model="formData.password"
+            required
+            class="w-full py-2 p-2 border rounded-lg mb-4"
           />
         </div>
         <div v-if="error" class="text-red-500">
           {{ error }}
         </div>
         <button
-            class="cursor-pointer w-full bg-indigo-500 text-white py-3 rounded-lg hover:bg-indigo-600 disabled:bg-gray-400"
-            type="submit"
-            :disabled="loading"
+          class="cursor-pointer w-full bg-indigo-500 text-white py-3 rounded-lg hover:bg-indigo-600 disabled:bg-gray-400"
+          type="submit"
+          :disabled="loading"
         >
           {{ loading ? "Connexion en cours..." : "Se connecter" }}
         </button>
       </div>
       <div class="p-4">
         Vous n’avez pas de compte ?
-        <NuxtLink class="underline text-indigo-500 font-semibold" to="/inscription">
+        <NuxtLink
+          class="underline text-indigo-500 font-semibold"
+          to="/compte/inscription"
+        >
           Créer un compte
         </NuxtLink>
       </div>
     </div>
   </form>
-
 </template>
 
 <script setup>
