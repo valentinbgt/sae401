c6e6e1c92a26997455004643fa65a799c7bff784
DraayZe <lenny.fernet7@gmail.com>
2025-03-19 16:53:12 +0100

    Responsive des pages inscription et connexion

--- pages/compte/connexion.vue
+++ pages/compte/connexion.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex max-w-[1920px]  h-screen">
-    <div class="w-1/2 flex flex-col justify-center items-center p-12">
+  <div class="flex max-w-[1920px] h-screen">
+    <div class="w-full lg:w-1/2 flex flex-col justify-center items-center p-12">
       <h2 class="text-6xl font-bold mb-2 text-center">Bon retour !</h2>
       <p class="text-gray-600 mb-6 text-center">
         Veuillez vous connecter en rentrant vos identifiants.
@@ -8,8 +8,8 @@
       <LoginForm />
     </div>
 
-    <div class="w-1/2 bg-indigo-500 relative overflow-hidden ">
-      <div class="absolute -bottom-10 -right-10 ">
+    <div class="hidden lg:flex w-1/2 bg-indigo-500 relative overflow-hidden">
+      <div class="absolute -bottom-10 -right-10">
         <img
             src="../../assets/images/logo_agenda.svg"
             alt="logo-agenda"
@@ -19,5 +19,3 @@
     </div>
   </div>
 </template>
-<script setup lang="ts">
-</script>
