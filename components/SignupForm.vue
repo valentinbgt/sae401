39ecaf6f36ec83b7007eb65b2bd96cbf612a6237
Jean-Michel Le <jean-michel.le@etudiant.univ-reims.fr>
2025-04-22 21:26:52 +0200

    correction du SignupForm

--- components/SignupForm.vue
+++ components/SignupForm.vue
@@ -1,75 +1,80 @@
 <template>
   <form @submit.prevent="submitSignup" class="w-full">
     <div class="mx-14">
-      <form @submit.prevent="submitSignup" class="w-full">
-        <div class="space-y-10">
-          <div class="flex flex-col xl:flex-row w-full xl:justify-between">
-            <div class="flex flex-col w-full xl:w-60">
-              <label class="font-bold" for="prenom">Prénom</label>
-              <input
-                class="border rounded-lg py-2 p-2"
-                type="text"
-                id="prenom"
-                v-model="formData.prenom"
-                required
-              />
-            </div>
-
-            <div class="flex flex-col w-full xl:w-60">
-              <label class="font-bold" for="nom">Nom</label>
-              <input
-                class="border rounded-lg py-2 p-2"
-                type="text"
-                id="nom"
-                v-model="formData.nom"
-                required
-              />
-            </div>
-          </div>
-
-          <div class="flex flex-col w-full">
-            <label class="font-bold" for="email">Email</label>
+      <div class="space-y-10">
+        <!-- Ligne pour Prénom et Nom -->
+        <div class="flex flex-col md:flex-row w-full md:justify-between md:space-x-6">
+          <div class="flex flex-col w-full md:w-1/2">
+            <label class="font-bold" for="prenom">Prénom</label>
             <input
-              class="border rounded-lg py-2 p-2"
-              type="email"
-              id="email"
-              v-model="formData.email"
+              class="border rounded-lg py-2 px-2"
+              type="text"
+              id="prenom"
+              v-model="formData.prenom"
               required
             />
           </div>
 
-          <div class="flex flex-col w-full">
-            <label class="font-bold" for="password">Mot de passe</label>
+          <div class="flex flex-col w-full md:w-1/2 mt-4 md:mt-0">
+            <label class="font-bold" for="nom">Nom</label>
             <input
-              type="password"
-              id="password"
-              v-model="formData.password"
+              class="border rounded-lg py-2 px-2"
+              type="text"
+              id="nom"
+              v-model="formData.nom"
               required
-              class="border rounded-lg py-2 p-2"
             />
           </div>
-
-          <div v-if="error" class="text-red-500">
-            {{ error }}
-          </div>
-
-          <button
-            class="border w-full bg-indigo-500 text-white rounded-lg py-3 cursor-pointer hover:bg-indigo-600 disabled:bg-gray-400"
-            type="submit"
-            :disabled="loading"
-          >
-            {{ loading ? "Inscription en cours..." : "S'inscrire" }}
-          </button>
         </div>
-        <div class="mt-4 text-center">
-          Déjà un compte ?
-          <NuxtLink
-            class="underline text-indigo-500 font-semibold"
-            to="connexion"
-            >Se connecter</NuxtLink
-          >
+
+        <!-- Email -->
+        <div class="flex flex-col w-full">
+          <label class="font-bold" for="email">Email</label>
+          <input
+            class="border rounded-lg py-2 px-2"
+            type="email"
+            id="email"
+            v-model="formData.email"
+            required
+          />
         </div>
-      </form>
+
+        <!-- Mot de passe -->
+        <div class="flex flex-col w-full">
+          <label class="font-bold" for="password">Mot de passe</label>
+          <input
+            type="password"
+            id="password"
+            v-model="formData.password"
+            required
+            class="border rounded-lg py-2 px-2"
+          />
+        </div>
+
+        <!-- Message d'erreur -->
+        <div v-if="error" class="text-red-500">
+          {{ error }}
+        </div>
+
+        <!-- Bouton d'inscription -->
+        <button
+          class="border w-full bg-indigo-500 text-white rounded-lg py-3 cursor-pointer hover:bg-indigo-600 disabled:bg-gray-400"
+          type="submit"
+          :disabled="loading"
+        >
+          {{ loading ? "Inscription en cours..." : "S'inscrire" }}
+        </button>
+      </div>
+
+      <!-- Lien vers la connexion -->
+      <div class="mt-4 text-center">
+        Déjà un compte ?
+        <NuxtLink
+          class="underline text-indigo-500 font-semibold"
+          to="connexion"
+          >Se connecter</NuxtLink
+        >
+      </div>
     </div>
   </form>
 </template>
