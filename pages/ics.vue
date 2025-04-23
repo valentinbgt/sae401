e706807732e3dcc9512e750f515adff600e52c00
DraayZe <lenny.fernet7@gmail.com>
2025-04-23 10:08:36 +0200

    Fix image de fond sur la page nouvelle date

--- pages/ics.vue
+++ pages/ics.vue
@@ -203,11 +203,11 @@
     </div>
   </div>
 
-  <div class="absolute z-[-1] -bottom-10 -right-10">
+  <div class="fixed bottom-0 -right-10 -mb-10 -ml-10 w-40 sm:w-40 md:w-52 lg:w-68 xl:w-100">
     <img
-      src="../assets/images/logo_agenda.svg"
-      alt="logo-agenda"
-      class="w-100"
+        src="../assets/images/logo_agenda.svg"
+        alt="logo-agenda"
+        class="w-full"
     />
   </div>
 </template>
