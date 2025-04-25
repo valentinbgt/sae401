618185ab71dc81d45c5c187db2489d7860d67081
Jean-Michel Le <jean-michel.le@etudiant.univ-reims.fr>
2025-04-25 15:20:50 +0200

    correction du nav  et admin

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,17 +1,50 @@
 <template>
   <TopNav :notif="true">Admin</TopNav>
-  <div class="flex gap-4 p-4 justify-center">
+  <div class="flex flex-wrap gap-4 p-4 justify-center">
+    <!-- Bouton Voir les modules -->
     <NuxtLink
       to="admin/modules"
-      class="w-1/6 p-3 bg-indigo-500 font-semibold rounded-lg text-white cursor-pointer hover:bg-indigo-600 disabled:bg-gray-400 flex items-center justify-center text-center"
+      class="w-full sm:w-1/3 lg:w-1/6 p-3 bg-indigo-500 font-semibold rounded-lg text-white cursor-pointer hover:bg-indigo-600 disabled:bg-gray-400 flex items-center justify-center text-center gap-2"
     >
-      Voir les modules</NuxtLink
-    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"
+        />
+      </svg>
+      Voir les modules
+    </NuxtLink>
+
+    <!-- Bouton Gérer les utilisateurs -->
     <NuxtLink
       to="admin/utilisateurs"
-      class="w-1/6 p-3 bg-indigo-500 font-semibold rounded-lg text-white cursor-pointer hover:bg-indigo-600 disabled:bg-gray-400 flex items-center justify-center text-center"
-      >Gérer les utilisateurs</NuxtLink
+      class="w-full sm:w-1/3 lg:w-1/6 p-3 bg-indigo-500 font-semibold rounded-lg text-white cursor-pointer hover:bg-indigo-600 disabled:bg-gray-400 flex items-center justify-center text-center gap-2"
     >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+        />
+      </svg>
+      Gérer les utilisateurs
+    </NuxtLink>
   </div>
 </template>
+
 <script setup lang="ts"></script>
